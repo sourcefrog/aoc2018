522db3eb14448c00ef46d67b3e31e7b6bde4cef8
Martin Pool <mbp@sourcefrog.net>
2018-12-04 19:56:37 -0800

    Extract read_ints function

--- src/bin/aoc1.rs
+++ src/bin/aoc1.rs
@@ -3,12 +3,16 @@
 use std::io;
 use std::io::prelude::*;
 
-pub fn main() {
-    let stdin = io::stdin();
-    let mut t = 0;
-    for line in stdin.lock().lines() {
-        let v = line.unwrap().parse::<i64>().unwrap();
-        t += v;
+/// Read a list of signed integers from stdin.
+fn read_ints() -> Vec<i64> {
+    let mut r = Vec::<i64>::new();
+    for line in io::stdin().lock().lines() {
+        r.push(line.unwrap().parse::<i64>().unwrap());
     }
+    r
+}
+
+pub fn main() {
+    let t: i64 = read_ints().iter().sum();
     println!("total: {}", t);
 }
