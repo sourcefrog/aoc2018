6bae51fc1c63a1c137a3cce691546455b62dc8c5
Martin Pool <mbp@sourcefrog.net>
2019-04-22 12:44:23 +1000

    New simpler attempt at 20a.
    Generate all possible paths by successively expanding out each alternate
    group.

--- src/bin/aoc20a.rs
+++ src/bin/aoc20a.rs
@@ -1,8 +1,7 @@
 #![allow(unused)]
 
-use std::collections::VecDeque;
-
 /// https://adventofcode.com/2018/day/20
+use std::collections::VecDeque;
 
 type Coord = i32;
 #[derive(Debug, PartialEq, Eq, Copy, Clone)]
@@ -11,134 +10,155 @@ struct Point {
     y: Coord,
 }
 
+/// Expand this regexp into a vector of every string that it can possibly match.
+///
+/// This is done by successively pulling off the first, top-level alternative group,
+/// and enqueueing a new string to expand, for each of those alternatives.
+/// This proceeds until there's nothing more to expand, at which point the string
+/// is added to the result list.
+fn expand(instr: &str) -> Vec<String> {
+    // TODO: Maybe work in terms of indexes into the instruction list, rather than
+    // copied strings?
+    // TODO: Maybe store the queue as a constant part followed by a paren
+    // part, to avoid repeatedly scanning the strings to find the first paren.
 
-#[derive(Debug)]
-struct Turtle {
-    /// Current location
-    p: Point,
-
-    /// Index into the instruction vector.
-    idx: usize,
-
-    /// The positions we were at when we saw the last open parens, and from which new siblings will
-    /// spawn.
-    open_p: Vec<Point>,
-
-    /// The paths we were at when we saw the last open parens.
-    open_path: Vec<String>,
-
-    /// Identifier number for this turtle to make them easier to track.
-    turtle_id: usize,
-
-    /// Path, for debugging.
-    path: String,
+    // Queue of partially-completed expansions that we need to revisit. Each
+    // might contain one or more alternates.
+    let mut queue: VecDeque<String> = VecDeque::new();
+    queue.push_back(instr.to_string());
+    let mut r = Vec::new();
+    while let Some(x) = queue.pop_front() {
+        let (head, alts, tail) = parse_alternate(x);
+        if alts.is_empty() {
+            // If there were no alternates, there can't be a tail after them.
+            assert!(tail.is_empty());
+            r.push(head);
+        } else {
+            for a in alts {
+                // Queue up each of these for later evaluation
+                let mut b = head.clone();
+                b.push_str(&a);
+                b.push_str(&tail);
+                queue.push_back(b);
+            }
+        }
+    }
+    r
 }
 
+/// Parse out the first alternate group from a string, returning the
+/// head (before the alternate), a vec of alternatives, and then the
+/// tail after it. Only the topmost alternative is parsed-out, so any of
+/// the alts might themselves contain alternatives that later need to be expanded.
+fn parse_alternate(s: String) -> (String, Vec<String>, String) {
+    let sb = s.as_bytes();
+    let mut i = 0;
+    let mut head = String::new();
+    // Pull off any head section before the parens
+    loop {
+        if i >= sb.len() {
+            return (s, vec![], String::new()); // contains no paren
+        } else if sb[i] == b'(' {
+            head = s[..i].to_string();
+            break;
+        }
+        i += 1;
+    }
+    // Skip over opening paren
+    assert_eq!(sb[i], b'(');
+    i += 1;
+
+    // Walk over alternates, accumulating each one into a vector, breaking
+    // on top-level pipes or the closing bracket.
+    let mut alts = Vec::new();
+    let mut level = 0;
+    let mut ai = i;
+    loop {
+        let c = sb[i];
+        if level == 0 {
+            match c {
+                b'(' => {
+                    level += 1;
+                }
+                b'|' => {
+                    alts.push(s[ai..i].to_string());
+                    ai = i + 1;
+                }
+                b')' => {
+                    // conclude the last alternate, then stop
+                    alts.push(s[ai..i].to_string());
+                    i += 1;
+                    break;
+                }
+                _other => (),
+            }
+        } else {
+            // not at the top level, so just count parens for the sake of grouping, and skip
+            // everything else.
+            level += match c {
+                b'(' => 1,
+                b')' => -1,
+                _other => 0,
+            }
+        }
+        i += 1;
+    }
+    let tail = s[i..].to_string();
+    (head, alts, tail)
+}
 
 fn walk(r: &str) {
     assert!(r.starts_with("^"));
     assert!(r.ends_with("$"));
     assert!(r.is_ascii());
-    let r = &r[1..(r.len()-1)];
+    let r = &r[1..(r.len() - 1)];
     println!("r inner = {:?}", r);
     let cs: Vec<char> = r.chars().collect();
-    run_top(&cs);
+    // run_top(&cs);
 }
-
-
-/// Return the index of the next unmatched closing paren after `i`, which must
-/// be inside an open paren.
-fn skip_to_close(i: usize, r: &[char]) -> usize {
-    let mut i = i;
-    let mut level = 0;
-    loop {
-        let c = r[i];
-        if c == ')' {
-            if level == 0 {
-                return i;
-            } else {
-                level -= 1;
-            }
-        } else if c == '(' {
-            level += 1;
-        }
-        i += 1;
-    }
-}
-
-
-fn run_top(r: &[char]) {
-    let mut saved: VecDeque<Turtle> = VecDeque::new();
-    let mut turtle_id = 0;
-    saved.push_back(Turtle {
-        p: Point { x: 0, y: 0 },
-        idx: 0,
-        open_p: Vec::new(),
-        open_path: Vec::new(),
-        turtle_id,
-        path: String::new(),
-    });
-    while let Some(mut t) = saved.pop_front() {
-        println!("** moving turtle {:?}, starting at {:?}", t.turtle_id, t.p);
-        let mut idx = t.idx;
-        while idx < r.len() {
-            let c = r[idx];
-            match c {
-                'N' | 'S' | 'E' | 'W' => {
-                    // Step in that direction, advance to next character.
-                    // TODO: Actually update the location.
-                    println!("{:?}", c);
-                    t.path.push(c);
-                },
-                '(' => {
-                    // Remember this location, so that we can later spawn from here.  Then,
-                    // continue this turtle walking down the first branch.
-                    t.open_p.push(t.p);
-                    t.open_path.push(t.path.clone());
-                },
-                '|' => {
-                    // But for this turtle, just skip to the end point of the last-saved
-                    // location.
-                    println!("looking at pipe at {:?}...", idx);
-                    let last_p = t.open_p.last().unwrap();
-                    turtle_id += 1;
-                    let newt = Turtle {
-                        p: *last_p,
-                        idx: idx + 1,
-                        open_p: t.open_p.clone(),
-                        open_path: t.open_path.clone(),
-                        turtle_id,
-                        path: t.open_path.last().unwrap().clone(),
-                    };
-                    println!("spawn new turtle {:?}", newt);
-                    // TODO: Avoid spawning exact-duplicate turtles for everyone that proceeds
-                    // past a branch. Just one is enough.
-                    saved.push_back(newt);
-                    idx = skip_to_close(idx, r);
-                    println!("skip to close paren at {:?}", idx);
-                    continue;
-                },
-                ')' => {
-                    // Whether we jumped here from a pipe, or reached here through natural
-                    // exhaustion, we're done. Forget about the last-opened paren and position,
-                    // and proceed onwards.
-                    println!("reached close paren at {}", idx);
-                    t.open_p.pop().unwrap();
-                    t.open_path.pop().unwrap();
-                },
-                other => panic!("unexpected instruction {:?}", other),
-            }
-            idx += 1;
-        }
-        println!("turtle {:?} completed, path was {:?}", t.turtle_id, t.path);
-    }
-}
-
 
 pub fn main() {
     //walk("^WNE$");
     // walk("^WNE(EEE(SS|NN)|WWW|)$");
     // walk("^WNE(EEE(SS|NN)|WWW)$");
-    walk("^W((EE|SS)|(NN|WW))$");
+    // walk("^W((EE|SS)|(NN|WW))$");
     // walk("^WNE(EEE|WWW|)$");
+}
+
+#[cfg(test)]
+mod test {
+    use super::expand;
+    use super::parse_alternate;
+
+    fn check_parse(s: &str, head: &str, alts: Vec<&str>, tail: &str) {
+        let alt_strings: Vec<String> = alts.into_iter().map(str::to_string).collect();
+        assert_eq!(
+            parse_alternate(s.to_string()),
+            (head.to_string(), alt_strings, tail.to_string())
+        );
+    }
+
+    #[test]
+    fn test_parse_alternate() {
+        check_parse("", "", vec![], "");
+        check_parse("ABC", "ABC", vec![], "");
+        check_parse("ABC()()", "ABC", vec![""], "()");
+        check_parse("ABC()", "ABC", vec![""], "");
+        check_parse("((N|S)|(E|W))", "", vec!["(N|S)", "(E|W)"], "");
+        check_parse("NEWS((N|E|)EE|WW)", "NEWS", vec!["(N|E|)EE", "WW"], "");
+        check_parse("NEWS(N|E|)EE", "NEWS", vec!["N", "E", ""], "EE");
+    }
+
+    #[test]
+    fn test_expand() {
+        assert_eq!(expand(""), vec![""]);
+        assert_eq!(expand("N"), vec!["N"]);
+        assert_eq!(expand("NEW"), vec!["NEW"]);
+        assert_eq!(expand("(N|E)"), vec!["N", "E"]);
+        assert_eq!(expand("((N|S)|(E|W))"), vec!["N", "S", "E", "W"]);
+        assert_eq!(
+            expand("NEWS((N|E|)EE|WW)"),
+            vec!["NEWSWW", "NEWSNEE", "NEWSEEE", "NEWSEE"]
+        );
+    }
 }
