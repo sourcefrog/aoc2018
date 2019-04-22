a8da7f568bf212728f51bb04aee1811710b5b051
Martin Pool <mbp@sourcefrog.net>
2019-04-22 13:02:44 +1000

    20a: Try on real input - it's too slow
    This algorithm generates too much queued-up state on the real input,
    with 10,000s of queued up paths to explore, even while the path length
    remains fairly modest at a couple of hundred steps. It seems we need a
    different approach.

--- src/bin/aoc20a.rs
+++ src/bin/aoc20a.rs
@@ -2,6 +2,8 @@
 
 /// https://adventofcode.com/2018/day/20
 use std::collections::VecDeque;
+use std::fs::File;
+use std::io::prelude::*;
 
 type Coord = i32;
 #[derive(Debug, PartialEq, Eq, Copy, Clone)]
@@ -27,12 +29,17 @@ fn expand(instr: &str) -> Vec<String> {
     let mut queue: VecDeque<String> = VecDeque::new();
     queue.push_back(instr.to_string());
     let mut r = Vec::new();
+    let mut ipaths = 0;
     while let Some(x) = queue.pop_front() {
         let (head, alts, tail) = parse_alternate(x);
         if alts.is_empty() {
             // If there were no alternates, there can't be a tail after them.
-            assert!(tail.is_empty());
-            r.push(head);
+            debug_assert!(tail.is_empty());
+            ipaths += 1;
+            if ipaths % 100000 == 1 {
+                println!("found {:?} paths, queue depth {}, recently {:?}", ipaths, queue.len(), head);
+            }
+            // r.push(head);
         } else {
             for a in alts {
                 // Queue up each of these for later evaluation
@@ -65,7 +72,7 @@ fn parse_alternate(s: String) -> (String, Vec<String>, String) {
         i += 1;
     }
     // Skip over opening paren
-    assert_eq!(sb[i], b'(');
+    debug_assert_eq!(sb[i], b'(');
     i += 1;
 
     // Walk over alternates, accumulating each one into a vector, breaking
@@ -117,6 +124,19 @@ fn walk(r: &str) {
     // run_top(&cs);
 }
 
+/// Return the canned input with newline, ^ and $ removed.
+fn load_input() -> String {
+    let mut s = String::with_capacity(100_000);
+    File::open("input/input20.txt")
+        .unwrap()
+        .read_to_string(&mut s)
+        .unwrap();
+    s.shrink_to_fit();
+    assert!(s.ends_with("$\n"));
+    assert!(s.starts_with("^"));
+    s[1..(s.len() - 2)].to_string()
+}
+
 pub fn main() {
     //walk("^WNE$");
     // walk("^WNE(EEE(SS|NN)|WWW|)$");
@@ -160,5 +180,18 @@ mod test {
             expand("NEWS((N|E|)EE|WW)"),
             vec!["NEWSWW", "NEWSNEE", "NEWSEEE", "NEWSEE"]
         );
+    }
+
+    #[test]
+    fn test_load_input() {
+        super::load_input();
+    }
+
+    #[test]
+    fn test_expand_input() {
+        let inst = super::load_input();
+        println!("{:?} bytes of input", inst.len());
+        let exp = expand(&inst);
+        println!("{:?} paths in output; first is {:?}", exp.len(), exp[0]);
     }
 }
