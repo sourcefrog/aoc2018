e36fb17077248f311330e87ff40d5bcf5531ca2c
Martin Pool <mbp@sourcefrog.net>
2019-08-12 07:44:56 +1000

    Split out Point source and add Point::neighbors

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,6 @@
+/// mbp AoC2018 solutions - general utilities.
 pub mod matrix;
+mod point;
+
 pub use crate::matrix::Matrix;
-
-use std::fmt;
-
-#[derive(Copy, Clone, PartialEq, Eq, Ord, PartialOrd)]
-pub struct Point {
-    pub y: usize,
-    pub x: usize,
-}
-
-impl fmt::Debug for Point {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "point({}, {})", self.x, self.y)
-    }
-}
-
-/// Shorthand to construct a point.
-pub fn point(x: usize, y: usize) -> Point {
-    Point { x, y }
-}
-
-impl Point {
-    pub fn down(&self) -> Point {
-        point(self.x, self.y.checked_add(1).unwrap())
-    }
-
-    pub fn left(&self) -> Point {
-        point(self.x.checked_sub(1).unwrap(), self.y)
-    }
-
-    pub fn right(&self) -> Point {
-        point(self.x.checked_add(1).unwrap(), self.y)
-    }
-
-    pub fn up(&self) -> Point {
-        point(self.x, self.y.checked_sub(1).unwrap())
-    }
-}
+pub use crate::point::{point, Point};
