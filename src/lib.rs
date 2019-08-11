6044485fd8bdcea6bfd5a971af1aca5bcb1d3823
Martin Pool <mbp@sourcefrog.net>
2019-08-12 07:52:17 +1000

    Add basic generic shortest_distance

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 /// mbp AoC2018 solutions - general utilities.
 pub mod matrix;
 mod point;
+mod shortest_path;
 
 pub use crate::matrix::Matrix;
 pub use crate::point::{point, Point};
+pub use crate::shortest_path::shortest_distance;
