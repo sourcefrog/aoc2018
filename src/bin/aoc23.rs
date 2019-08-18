80a47f388763d2a3c2b40cc86a1c9a3f92cbfac2
Martin Pool <mbp@sourcefrog.net>
2019-08-18 12:18:50 +1000

    23b: Incorrect answer

--- src/bin/aoc23.rs
+++ src/bin/aoc23.rs
@@ -40,51 +40,40 @@
 //
 // (X-x).abs() + (Y-y).abs() + (Z-z).abs() <= r
 //
-// Start with Z=z to reduce it to the 2D case. Also, start at the
-// X>x, Y>y case, so the abs terms go away.
+// Start with Z=z to reduce it to the 2D case. Also, start at the X>x, Y>y
+// case, so the abs terms go away.
 //
-// (X - x) + (Y - y) <= r
-// X + Y <= r + x + y
+// (X - x) + (Y - y) <= r X + Y <= r + x + y
 //
-// then if X<x, Y>y
-// (x-X) + (Y-y) <= r
-// -X + Y <= r - x + y
+// then if X<x, Y>y (x-X) + (Y-y) <= r -X + Y <= r - x + y
 //
-// similarly
-// X - Y <= r + x - y
-// -X - Y <= r - x - y
+// similarly X - Y <= r + x - y -X - Y <= r - x - y
 //
-// These four constraints define a quadrilateral with unit slopes in a plane
-// of Z=z.
+// These four constraints define a quadrilateral with unit slopes in a plane of
+// Z=z.
 //
 // Expading to Z>z,
 //
-//  X + Y + Z   <= r + x + y + z
-// -X + Y + Z   <= r - x + y + z
-//  X - Y + Z   <= r + x - y + z
-//  ...
+//  X + Y + Z   <= r + x + y + z -X + Y + Z   <= r - x + y + z X - Y + Z   <= r
+//  + x - y + z ...
 //
-// So there are eight planes constraining the space, and they're all defined
-// by simple combinations of (x,y,z,r). Of course, they have to be.
+// So there are eight planes constraining the space, and they're all defined by
+// simple combinations of (x,y,z,r). Of course, they have to be.
 //
 // How do could we intersect two zones to find whether there is any resulting
-// zone?
-// First consider the simple one-dimensional case, (x1,r1) and (x2,r2)
+// zone?  First consider the simple one-dimensional case, (x1,r1) and (x2,r2)
 // where x1 <= x2.
 //
 // If (x1+r1) < (x2-r2) they do not touch; there is no intersection.
 //
-// aaaaAaaaa             x1 = 4, r1 = 4
-//       bbbbbbBbbbbbb   x2 = 12, r2 = 6
+// aaaaAaaaa             x1 = 4, r1 = 4 bbbbbbBbbbbbb   x2 = 12, r2 = 6
 //
-// Otherwise, there is an intersection of length di=((x1+r1) - (x2-r2)).
-// and with radius (treating the edge as included) of ri = di/2.
-// Implies r1 = (x1 + r1 - x2 + r2) / 2.
-// The center is at (x1 + r1 - ri).
+// Otherwise, there is an intersection of length di=((x1+r1) - (x2-r2)).  and
+// with radius (treating the edge as included) of ri = di/2.  Implies r1 = (x1
+// + r1 - x2 + r2) / 2.  The center is at (x1 + r1 - ri).
 //
-// This needs care with regard to off-by-one errors. How do we cope if
-// di is even? Maybe in that case it cannot be represented as
-// (x,y,z,r)?
+// This needs care with regard to off-by-one errors. How do we cope if di is
+// even? Maybe in that case it cannot be represented as (x,y,z,r)?
 //
 // And, in fact, this has another unhandled edge case: suppose x1=x2 but r2>r1.
 //
@@ -109,10 +98,59 @@
 //
 // Now, how can we tell if the zone is empty? Equivalently, r<0?
 //
-// pxpypz = x + y + z + r
-// mxmymz = -x - y - z + r
+// pxpypz = x + y + z + r mxmymz = -x - y - z + r
 //
 // pxpypz + mxmymz = 2r
+//
+// Now, moving on to finding the zone, and the closest-to-origin point, that
+// intersects the zones of the largest number of bots.
+//
+// One way to approach this is to find the zones that intersect between all
+// combinations of bots, pruning off combinations that have no intersection.
+// The problem with this is that there are up to 2**1000 possible combinations
+// of bots, and there are many overlaps between them, so not much can be pruned
+// out.
+//
+// We could take the same approach but work from the other end and see how many
+// need to be removed to find an intersection, although that might also blow
+// up.
+//
+// I wonder if it would help to cluster bots at similar locations, or deal
+// first with the largest ones?
+//
+// So we can consider this as a function over x,y,z where h(x,y,z) gives the
+// number of bots in range at any point. We want to find the point with the
+// maximum x,y,z. The values can be considered to stack up as we go along.
+//
+// There are vary many possible x,y,z because the ranges are so large, but only
+// some of them can be interesting: the ones at the edge of a zone boundary. No
+// new ones can possibly be introduced.
+//
+// (In fact, is it only the edges closest to the origin that are interesting?)
+//
+// Each bot defines a diamond with planes defined by eight parameters. The
+// diamond has six corners.  Are the interesting points always at one of these
+// corners? If so, perhaps it's easy to see which other bots might contain
+// them.
+//
+// 82010378 is too low...
+//
+// Just checking the corners, it seems, is not enough: it gets an answer that
+// is too low (too close to the origin.) Perhaps, there's another position
+// further away that intersects with more diamonds.
+//
+// Perhaps checking only the corners is not enough: when there are multiple
+// overlapping squares I can roughly imagine how the most-covered point
+// wouldn't be exactly on a corner. Must it be on an edge? It would be nice to
+// have a rigorous argument that it must.
+//
+// Return to the concept of drawing a graph along one axis, and looking for the
+// maximum stacking. It still seems it must occur on corners?
+//
+// I wonder if I should be looking for intersections between all the planes,
+// rather than just the corners? But, I can't yet think of any case where that
+// demonstrates any difference, or where the positions could be other than at
+// all the corners.
 
 extern crate itertools;
 extern crate regex;
@@ -146,6 +184,21 @@ impl Bot {
             mxmypz: -self.x - self.y + self.z + self.r,
             mxmymz: -self.x - self.y - self.z + self.r,
         }
+    }
+
+    fn corners(&self) -> [(isize, isize, isize); 6] {
+        [
+            (self.x - self.r, self.y, self.z),
+            (self.x + self.r, self.y, self.z),
+            (self.x, self.y - self.r, self.z),
+            (self.x, self.y + self.r, self.z),
+            (self.x, self.y, self.z - self.r),
+            (self.x, self.y, self.z + self.r),
+        ]
+    }
+
+    fn contains_point(&self, p: (isize, isize, isize)) -> bool {
+        (self.x - p.0).abs() + (self.y - p.1).abs() + (self.z - p.2).abs() <= self.r
     }
 }
 
@@ -199,9 +252,9 @@ fn load_input() -> Vec<Bot> {
 /// The teleportation zone of one or more bots.
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 struct Zone {
-    // The zone is expressed as the inclusive maximum value of
-    // a sum of the positive or negative values of x, y, and z. Within the zone
-    // all of these constraints are satisfied.
+    // The zone is expressed as the inclusive maximum value of a sum of the
+    // positive or negative values of x, y, and z. Within the zone all of these
+    // constraints are satisfied.
     pxpypz: isize,
     pxpymz: isize,
     pxmypz: isize,
@@ -266,8 +319,48 @@ fn solve_a() -> usize {
     count_in_range(&load_input())
 }
 
+fn solve_b() -> isize {
+    let bots = load_input();
+    let bp = find_most_covered(&bots);
+    bp.0.abs() + bp.1.abs() + bp.2.abs()
+}
+
+fn find_most_covered(bots: &[Bot]) -> (isize, isize, isize) {
+    let mut best_points = Vec::new();
+    let mut best_matches = 0;
+
+    // Number of matches at that point, and the point.
+    let mut res: Vec<(usize, (isize, isize, isize))> = Vec::new();
+
+    for (_i, b) in bots.iter().enumerate() {
+        // dbg!(i, b);
+        for corn in b.corners().iter() {
+            debug_assert!(b.contains_point(*corn));
+            let matches = bots.iter().filter(|b| b.contains_point(*corn)).count();
+            res.push((matches, *corn));
+            // dbg!(corn, matches);
+            if matches > best_matches {
+                best_points.clear();
+                best_points.push(*corn);
+                best_matches = matches;
+            } else if matches == best_matches {
+                best_points.push(*corn);
+            }
+        }
+    }
+    res.sort();
+    res.reverse();
+    dbg!(&res[..10]);
+    dbg!(&best_points, &best_matches);
+    assert_eq!(best_points.len(), 1);
+    let bp = best_points[0];
+    dbg!(bp.0 + bp.1 + bp.2);
+    bp
+}
+
 pub fn main() {
-    dbg!(solve_a());
+    // dbg!(solve_a());
+    println!("Solution to B: {}", solve_b());
 }
 
 #[cfg(test)]
@@ -346,6 +439,20 @@ mod tests {
         dbg!(inter_zone.r_values());
         assert!(inter_zone.contains_point((12, 12, 12)));
         assert_eq!(inter_zone.r_values(), (0, 0, 0, 0));
+    }
+
+    #[test]
+    fn example_2() {
+        let v = "\
+        pos=<10,12,12>, r=2
+        pos=<12,14,12>, r=2
+        pos=<16,12,12>, r=4
+        pos=<14,14,14>, r=6
+        pos=<50,50,50>, r=200
+        pos=<10,10,10>, r=5\
+        ";
+        let bots = super::parse(v);
+        assert_eq!(super::find_most_covered(&bots), (12, 12, 12));
     }
 
 }
