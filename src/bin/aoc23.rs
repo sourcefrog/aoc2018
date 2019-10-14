e56164581cf1e82054fcdc06bf71c330a80e18d1
Martin Pool <mbp@sourcefrog.net>
2019-10-14 11:21:10 -0700

    23: remove dead code

--- src/bin/aoc23.rs
+++ src/bin/aoc23.rs
@@ -60,12 +60,6 @@ impl Bot {
     fn contains_point(&self, p: (isize, isize, isize)) -> bool {
         (self.x - p.0).abs() + (self.y - p.1).abs() + (self.z - p.2).abs() <= self.r
     }
-
-    /// Return the Manhattan distance between two bots.
-    #[allow(unused)]
-    fn distance(&self, b: &Bot) -> isize {
-        (self.x - b.x).abs() + (self.y - b.y).abs() + (self.z - b.z).abs()
-    }
 }
 
 /// Parse an input string containing bot position descriptions into a vec of
@@ -163,17 +157,6 @@ impl Zone {
             || (self.pxmymz + self.mxpypz) < 0
     }
 
-    /// Returns four "radiuses" along each axis. If 0, there's only one
-    /// possible value on that axis; if negative there are no possible values.
-    fn r_values(&self) -> (isize, isize, isize, isize) {
-        (
-            self.pxpypz + self.mxmymz,
-            self.pxpymz + self.mxmypz,
-            self.pxmypz + self.mxpymz,
-            self.pxmymz + self.mxpypz,
-        )
-    }
-
     /// Return a coordinate in this zone that's closest to the origin.
     fn closest_to_origin(&self) -> Coord {
         let xmax1 = (self.pxpypz + self.pxmymz) / 2;
@@ -241,62 +224,6 @@ fn solve_a() -> usize {
 
 fn distance_from_origin(p: Coord) -> isize {
     p.0.abs() + p.1.abs() + p.2.abs()
-}
-
-// Return the number of bots that can reach p.
-#[allow(unused)]
-fn count_coverage(bots: &[Bot], p: Coord) -> usize {
-    bots.iter().filter(|b| b.contains_point(p)).count()
-}
-
-// Return the distance from the origin of the point closest to the origin that is covered by the
-// most bots, and that point.
-fn find_most_covered(bots: &[Bot]) -> (isize, Coord) {
-    // `cov[i]` is a set of all so-far-known zones covered by `i+1`
-    // bots.
-    let mut cov: Vec<BTreeSet<Zone>> = vec![BTreeSet::new(); bots.len()];
-
-    for (i, b) in bots.iter().enumerate() {
-        let mut limit = 1000;
-        // dbg!(i, b);
-        // For every previously-found cover, let's add b to it, to see if we can generate some
-        // higher-level covering zones.
-        let z = b.zone();
-        'bot: for j in (0..i).rev() {
-            let newent: Vec<Zone> = cov[j]
-                .iter()
-                .map(|oz| oz.intersect(&z))
-                .filter(|iz| !iz.is_empty())
-                .collect();
-            // dbg!(j + 1, &newent.len());
-            for nz in newent {
-                // If this exact same zone is already in level j, remove it: we only need to
-                // track it at j+1.
-                cov[j].remove(&nz);
-                cov[j + 1].insert(nz);
-                limit -= 1;
-                if limit == 0 {
-                    break 'bot;
-                }
-            }
-            if j + 1 == i {
-                // dbg!(&cov[j + 1]);
-            }
-        }
-
-        // Now let's add new layer 0 zones, covered only by this.
-        cov[0].insert(z);
-    }
-
-    // TODO: The highest populated value in `cov` is the solution.
-    for j in (0..cov.len()).rev() {
-        if !cov[j].is_empty() {
-            dbg!(j, &cov[j]);
-            let best_point = cov[j].iter().next().unwrap().closest_to_origin();
-            return (distance_from_origin(best_point), best_point);
-        }
-    }
-    unreachable!();
 }
 
 fn solve_b() -> isize {
@@ -445,22 +372,6 @@ mod tests {
             .fold1(|az, bz| az.intersect(&bz))
             .unwrap();
         dbg!(inter_zone);
-        dbg!(inter_zone.r_values());
         assert!(inter_zone.contains_point((12, 12, 12)));
-        assert_eq!(inter_zone.r_values(), (0, 0, 0, 0));
-    }
-
-    #[test]
-    fn example_2() {
-        let v = "\
-        pos=<10,12,12>, r=2
-        pos=<12,14,12>, r=2
-        pos=<16,12,12>, r=4
-        pos=<14,14,14>, r=6
-        pos=<50,50,50>, r=200
-        pos=<10,10,10>, r=5\
-        ";
-        let bots = super::parse(v);
-        assert_eq!(super::find_most_covered(&bots), (36, (12, 12, 12)));
     }
 }
